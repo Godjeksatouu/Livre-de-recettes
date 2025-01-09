721d6521d01120539cf7c1b7432c0116e71714fa
Godjeksatouu <godjeksatou@gmail.com>
2025-01-09 09:59:42 +0100

    app js

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import Layout from "./layout/Layout";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import GoToTheTop from "./components/GoToTheTop";
+
+function App() {
+  return (
+    <>
+      <Layout />
+      <ToastContainer position="top-center" />
+      <GoToTheTop />
+    </>
+  );
+}
+
+export default App;
