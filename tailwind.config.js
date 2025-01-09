3f14282dbab5b1f6fe0dae1957d8a8adbe066cc0
Godjeksatouu <godjeksatou@gmail.com>
2025-01-09 09:45:30 +0100

    root colors

---
+++ tailwind.config.js
@@ -0,0 +1,41 @@
+export default {
+  content: ["./index.html", "./src/**/*.jsx"],
+  theme: {
+    extend: {
+      fontFamily: {
+        marcellus: "Marcellus",
+      },
+      colors: {
+        customWhite: "#F2F1EC",
+        customGreen: "#345333",
+        customYellow: "#f2c94c",
+        customLightGray: "#f2f2ec",
+        customBorderColor: "#e9e9e9",
+        customBlack: "#212529",
+        customRed: "#F95755",
+        borderColorCard: "#e4e4e4",
+        categoryFilterColor: "#8b8b94",
+        titleGreen: "#4DA537",
+      },
+      container: {
+        center: true,
+        screens: {
+          sm: "640px",
+          md: "768px",
+          lg: "1024px",
+          lg: "1024px",
+          xl: "1280px",
+        },
+      },
+      backgroundImage: {
+        "hero-image": "url('./src/assets/images/1.jpg')",
+      },
+    },
+  },
+  plugins: [
+    function ({ addVariant }) {
+      addVariant("child", "& > *");
+      addVariant("child-hover", "& > *:hover");
+    },
+  ],
+};
